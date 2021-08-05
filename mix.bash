eba115e455ecb66dada2c507efa6c0db046367f3
Shlomi Fish <shlomif@shlomifish.org>
2021-08-05 09:35:28 +0300

    working

--- mix.bash
+++ mix.bash
@@ -1,8 +1,3 @@
 #!/bin/bash
-rm -f yonqatan.ogv yonqatan.webm
-# ffmpeg -loop 1 -i stuffed-dice.png -i yonqatan.mp3 -acodec vorbis -vcodec theora -shortest -q:v 5 -s 640x480 yonqatan.ogv
-ffmpeg \
-    -i stuffed-dice.png -i yonqatan.mp3 \
-    -s 640x480 yonqatan.webm
-# -acodec libvorbis -vcodec VP9 \
-# -loop 1 \
+rm -f grimmie-cover.webm
+ffmpeg -loop 1 -i 14922538317_9528231ea1_c.bmp -i Recording_26.m4a -acodec libvorbis  -shortest -q:v 5 -s 640x480 grimmie-cover.webm
