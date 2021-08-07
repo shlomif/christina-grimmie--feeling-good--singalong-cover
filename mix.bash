ed71eedb1a4a329a54900127871eed20d9cc369f
Shlomi Fish <shlomif@shlomifish.org>
2021-08-07 20:47:16 +0300

    fix firefox playback

--- mix.bash
+++ mix.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 rm -f grimmie-cover.webm
-ffmpeg -loop 1 -i 14922538317_9528231ea1_c.bmp -i Recording_26.m4a -acodec libvorbis  -shortest -q:v 5 -s 640x480 grimmie-cover.webm
+# ffmpeg -loop 1 -i 14922538317_9528231ea1_c.bmp -i Recording_26.m4a -acodec libvorbis  -shortest -q:v 5 -s 640x480 grimmie-cover.webm
+# ffmpeg -loop 1 -i 14922538317_9528231ea1_c.bmp -i Recording_26.m4a  -shortest -q:v 5 -s 640x480 grimmie-cover.webm
+ffmpeg -loop 1 -i 14922538317_9528231ea1_c.bmp -i Recording_26.m4a  -shortest -q:v 5 -acodec libvorbis -vcodec vp8 -s 640x480 grimmie-cover.webm
